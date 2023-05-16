479ca0aaad1aafad10b0f6699eba446413e3ddf6
Omar622 <omaramin622@gmail.com>
2023-05-16 13:16:31 +0300

    Create form to submit messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,36 @@
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  }
+];
+
 const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages: messages });
 });
+
+/* GET, POST form page. */
+router.route('/new')
+  .get((req, res) => {
+    res.render('form');
+  })
+  .post((req, res) => {
+    messages.push({
+      text: req.body.message,
+      user: req.body.user,
+      added: new Date()
+    });
+    res.redirect('/');
+  });
 
 module.exports = router;
